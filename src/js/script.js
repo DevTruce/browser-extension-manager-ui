11cfe47aeb8dd0361b3ebfd028b68ce0ce00a0f3
DevTruce <DevTruce@gmail.com>
2025-03-27 22:08:50 -0400

    update main padding for all screen sizes

--- src/js/script.js
+++ src/js/script.js
@@ -81,7 +81,7 @@ async function fetchJsonData(url) {
 // dynamically build html needed for card components
 function generateCardHTML(imagePath, name, description, isActive, i) {
   const cardHTML = `<div
-              class="card flex flex-col rounded-2xl bg-neutral0 p-5 justify-between space-y-12 outline outline-1 outline-neutral200 shadow-md dark:bg-neutral800 dark:outline-neutral600" data-id="${i}" data-state="${
+              class="card flex flex-col rounded-2xl bg-neutral0 p-5 justify-between space-y-12 outline outline-1 outline-neutral200 shadow-md overflow-scroll dark:bg-neutral800 dark:outline-neutral600" data-id="${i}" data-state="${
     isActive ? 'active' : 'inactive'
   }"
             >
